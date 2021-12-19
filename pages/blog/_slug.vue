0acabfaca507c81e7d85baa28a50103a04f8c5df
yuki-b4 <capou0872@gmail.com>
2021-12-20 07:36:30 +0900

    サンプルの作成と、コンテントフルの導入

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="slug">
+    <h1 class="slug_title">
+      タイトル
+    </h1>
+    <p class="slug_date">2018/8/2</p>
+    <div>
+      記事の内容。hogehogehogehogehogehogehogehoge
+    </div>
+  </section>
+</template>
